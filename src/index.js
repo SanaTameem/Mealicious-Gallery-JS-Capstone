1814bb5e8b159a7a77dd5cb518080508c17d1321
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-07-03 19:55:01 +0430

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,53 @@
 import './style.css';
-// eslint-disable-next-line
-const apiData = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=American';
-// eslint-disable-next-line
-const likeUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/T1s4xFdr6wtV60QfASVp/likes/';
-// eslint-disable-next-line
-const commentUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/T1s4xFdr6wtV60QfASVp/comments/';
+import getMeals from './modules/getMeals.js';
+
+const galleryContainer = document.querySelector('.gallery-container');
+let mealsArr = [];
+const showCards = async () => {
+  try {
+    mealsArr = await getMeals();
+    // const likeData = await getLikes();
+    mealsArr.forEach((meal, id) => {
+      const itemDiv = document.createElement('div');
+      itemDiv.classList.add('gallery-item');
+      itemDiv.setAttribute('data-id', id);
+      itemDiv.innerHTML = `
+        <div class="img-container">
+          <img src="${meal.strMealThumb}" alt="logo">
+        </div>
+        <div class="like-comment">
+          <p class="item-name">${meal.strMeal}</p>
+          <i class="fa-regular fa-heart" style="color: #ff0000;"></i>
+        </div>
+        <div class="number-of-likes"></div>
+        <div class="button-container">
+          <button class="comment-btn">Comments</button>
+          <button class="reservation-btn">Reservation</button>
+        </div>
+      `;
+
+      //   const numOfLikeDiv = itemDiv.querySelector('.number-of-likes');
+      //   let mealLikes = 0;
+      //   const like = likeData.find((like) => like.item_id === id);
+
+      //   if (like) {
+      //     mealLikes = like.likes;
+      //   }
+      //   numOfLikeDiv.textContent = `${mealLikes} Likes`;
+      galleryContainer.appendChild(itemDiv);
+    //   const commentBtns = itemDiv.querySelectorAll('.comment-btn');
+    //   commentBtns.forEach((btn) => {
+    //     btn.addEventListener('click', () => {
+    //       popUp(meal.idMeal);
+    //       commentGet(meal.idMeal);
+    //     });
+    //   });
+    });
+  } catch (error) {
+    console.error('Error showing cards:', error);
+  }
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
+  await showCards();
+});
