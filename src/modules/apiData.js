85f75b20fcd726cc162b13ef5a8ae6970aa14094
kit0-0 <kt1767@gmail.com>
2023-07-04 13:50:12 +0530

    Add popup and remove comments

--- src/modules/apiData.js
+++ src/modules/apiData.js
@@ -1,7 +1,4 @@
-// eslint-disable-next-line
 const apiData = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=American';
-// eslint-disable-next-line
 const likeUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/T1s4xFdr6wtV60QfASVp/likes/';
-// eslint-disable-next-line
 const commentUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/T1s4xFdr6wtV60QfASVp/comments/';
 export { apiData, likeUrl, commentUrl };
